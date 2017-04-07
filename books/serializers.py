b90c479e9f34bee03bac5b1efc152780495e92f5
Marina M <xenonsumback@gmail.com>
2017-04-07 19:13:09 +0600

    added ManyToMany fields

--- books/serializers.py
+++ books/serializers.py
@@ -5,16 +5,16 @@ from rest_framework import serializers
 class BookSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Books
-        fields = ('name_book', 'description', 'ISBN', 'publishing_house', 'year', 'quantity')
+        fields = ('id', 'name_book', 'author', 'tags' 'description', 'ISBN', 'publishing_house', 'year', 'quantity')
 
 
 class AuthorSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Authors
-        fields = ('name',)
+        fields = ('id', 'name',)
 
 
 class TagsSerializer (serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Tags
-        fields = ('tag_name',)
+        fields = ('id', 'tag_name',)
