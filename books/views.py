7e4f8f0cdc895b222b1d945d4088eb23b900857e
Marina M <xenonsumback@gmail.com>
2017-05-12 17:38:57 +0600

    added view for book(get)

--- books/views.py
+++ books/views.py
@@ -1,17 +1,31 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+
 from models import Books, Authors, Tags
 from rest_framework import viewsets
 from serializers import BookSerializer, AuthorSerializer, TagsSerializer
 
 
-class BookViewSet(viewsets.ModelViewSet):
+class BookViewSet(viewsets.ViewSet):
     """
     API endpoint that allows books to be viewed or edited.
     """
     queryset = Books.objects.all()
     serializer_class = BookSerializer
+
+    def list(self, request):
+        queryset = Books.objects.all()
+        serializer = BookSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Books.objects.all()
+        book = get_object_or_404(queryset, pk=pk)
+        serializer = BookSerializer(book)
+        return Response(serializer.data)
 
 
 class AuthorViewSet(viewsets.ModelViewSet):
