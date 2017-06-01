62fa69bab738ecf84dfd8d16162701e548c20b5d
Marina M <xenonsumback@gmail.com>
2017-06-01 18:15:50 +0600

    Add registration view.(troubles with permissions)

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
+
 from users import views
 
 
 urlpatterns = [
     url(r'^login/$', views.user_auth),
+    url(r'^registration/$', views.registration),
+    url(r'^logout/$', views.logout_view),
+    url(r'test/$', views.test)
 ]
--- users/views.py
+++ users/views.py
@@ -1,12 +1,14 @@
 # Create your views here.
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.views import logout
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import viewsets
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
 
 from serializers import UserSerializer
+from forms import RegistrationForm
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -17,34 +19,53 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
 
 
+@api_view(['GET'])
+@authentication_classes([])
+@permission_classes([])
+@csrf_exempt
+def logout_view(request):
+    logout(request)
+    content = {
+        'redirect': unicode('/login'),
+    }
+    return Response(content)
+
+
 @api_view(['POST'])
+@authentication_classes([])
+@permission_classes([])
 @csrf_exempt
 def user_auth(request):
     username = request.POST.get('username', None)
     password = request.POST.get('password', None)
     content = {
-        'user': unicode(request.user),  # `django.contrib.auth.User` instance.
+        'user': unicode(username),  # `django.contrib.auth.User` instance.
     }
     user = authenticate(username=username, password=password)
     if user is not None:
         if user.is_active:
             login(request, user)
             return Response(content)
-            # Redirect to a success page.)
-    # Return a 'disabled account' error message
     return Response(content)
 
-# Return an 'invalid login' error message.
 
 
 @api_view(['POST'])
-# @authentication_classes((SessionAuthentication, BasicAuthentication))
-# @permission_classes((IsAuthenticated,))
 @authentication_classes([])
 @permission_classes([])
-def example_view(request):
-    content = {
-        'user': unicode(request.user),  # `django.contrib.auth.User` instance.
-        'auth': unicode(request.auth),  # None
-    }
-    return Response(content)
+@csrf_exempt
+def registration(request):
+    username = request.data.get('username', None)
+    email = request.data.get('email', None)
+    password = request.data.get('password', None)
+    confirm_password = request.POST.get('confirm_password', None)
+    form = RegistrationForm(request.data)
+    if form.is_valid():
+        user = User(username=username, email=email)
+        user.set_password(password)
+        user.save()
+        content = {
+            'redirect': unicode('/login'),
+        }
+        return Response(content)
+    return Response(form.errors.as_json())
